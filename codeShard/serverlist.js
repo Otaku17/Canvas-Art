de3667250949e44b22249c7ccc1d6a0eecc55564
Ota <stevenotaku17@gmail.com>
2019-02-24 23:17:39 +0100

    Update serverlist.js

--- codeShard/serverlist.js
+++ codeShard/serverlist.js
@@ -13,15 +13,15 @@ module.exports.run = async(client, message) => {
             if (reaction.emoji.name === "⬅") {
                 p0 = p0-10;
                 p1 = p1-10;
-                if(p0 < 0) return data.delete();
-                if(p0 === undefined || p1 === undefined) return data.delete();
+                if(p0 < 0) { return data.delete(); }
+                if(p0 === undefined || p1 === undefined) { return data.delete(); }
                 data.edit(client.guilds.sort((a,b)=>b.memberCount-a.memberCount).map(r=>r).map((r, i) => `[${i + 1}] • Name: \`${r.name.toString()}\`\nUsers: \`${r.memberCount}\``).slice(p0, p1).join('\n'));
             }
             if (reaction.emoji.name === "➡") {
                 p0 = p0+10;
                 p1 = p1+10;
-                if(p1 > client.guilds.size + 10) return data.delete();
-                if(p0 === undefined || p1 === undefined) return data.delete();
+                if(p1 > client.guilds.size + 10) { return data.delete(); }
+                if(p0 === undefined || p1 === undefined) { return data.delete(); }
                 data.edit(client.guilds.sort((a,b)=>b.memberCount-a.memberCount).map(r=>r).map((r, i) => `[${i + 1}] • Name: \`${r.name.toString()}\`\nUsers: \`${r.memberCount}\``).slice(p0, p1).join('\n'));
             }
             if (reaction.emoji.name === "❌") { return data.delete(data); }
